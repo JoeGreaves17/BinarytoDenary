5cf528927c3283769b952353019bfa8bbc914cb3
JoeGreaves17 <An@kin20>
2020-11-13 13:52:37 +0000

    First upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the bit with weight 8:");
+        int digitValue8 = Integer.parseInt(input.nextLine());
+        System.out.println("Please enter the bit with weight 4:");
+        int digitValue4 = Integer.parseInt(input.nextLine());
+        System.out.println("Please enter the bit with weight 2:");
+        int digitValue2 = Integer.parseInt(input.nextLine());
+        System.out.println("Please enter the bit with weight 1:");
+        int digitValue1 = Integer.parseInt(input.nextLine());
+        System.out.println("Denary: " + String.valueOf((digitValue8 * 8) + (digitValue4 * 4) + (digitValue2 * 2) + digitValue1));
+    }
+}
